d015b8d69057b8b2cf67268c3e0938ea100ac2c4
inflames2k <inflames2k@web.de>
2024-12-15 10:49:21 +0100

    Update extended_admincall.js

--- extended_admincall.js
+++ extended_admincall.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Extended Admincall
 // @namespace    http://ps.addins.net/
-// @version      2.7
+// @version      2.7.1
 // @author       riesaboy
 // @match        https://*.knuddels.de:8443/ac/*
 // @icon         data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==
@@ -348,6 +348,8 @@ class BaseVariables
           $(this).empty().append('<textarea style="width: 100%; height: 100px;">' + text + '</textarea>');}
         else if(index == 3)
           $(this).empty().append('<a href="#" class="confirmEdit' + warning.id + '">✔️</a>');
+        else if(index == 4)
+          $(this).empty();
         index++;
       });
 
