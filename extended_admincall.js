ddbf5cfee734c1b5233a5a057de312328ce473b9
inflames2k <inflames2k@web.de>
2024-12-15 14:29:39 +0100

    Update extended_admincall.js

--- extended_admincall.js
+++ extended_admincall.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Extended Admincall
 // @namespace    http://ps.addins.net/
-// @version      2.7.7
+// @version      2.7.8
 // @author       riesaboy
 // @match        https://*.knuddels.de:8443/ac/*
 // @icon         data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==
@@ -1161,7 +1161,6 @@ class BaseVariables
       setReportLinks();
       addTextFilter();
       addReportNames();
-
     }
 
     function addReportNames()
